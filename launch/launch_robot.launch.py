ac53f12f1e7139d14cde7908f579c68a33917488
Lavanya2104 <lavanyamittal07@gmail.com>
2025-01-16 23:06:35 +0530

    Get Real Robot working

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -1,70 +1,40 @@
 import os
-
 from ament_index_python.packages import get_package_share_directory
-
-
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, TimerAction
+from launch.actions import IncludeLaunchDescription, TimerAction, RegisterEventHandler
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command
-from launch.actions import RegisterEventHandler
-from launch.event_handlers import OnProcessStart
-
 from launch_ros.actions import Node
-
-
-
+from launch.event_handlers import OnProcessStart
 def generate_launch_description():
-
-
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
-    package_name='articubot_one' #<--- CHANGE ME
+    package_name = 'tur_bot'
 
     rsp = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory(package_name), 'launch', 'rsp.launch.py'
+        )]), launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
     )
 
-    # joystick = IncludeLaunchDescription(
-    #             PythonLaunchDescriptionSource([os.path.join(
-    #                 get_package_share_directory(package_name),'launch','joystick.launch.py'
-    #             )])
-    # )
-
-
-    twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
-    twist_mux = Node(
-            package="twist_mux",
-            executable="twist_mux",
-            parameters=[twist_mux_params],
-            remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
-        )
-
-    
-
-
-    robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
-
-    controller_params_file = os.path.join(get_package_share_directory(package_name),'config','my_controllers.yaml')
-
+    # Load robot controller parameters
+    controller_params_file = os.path.join(get_package_share_directory(package_name), 'config', 'my_controllers.yaml')
+   
+    # Controller manager for robot control
     controller_manager = Node(
         package="controller_manager",
         executable="ros2_control_node",
-        parameters=[{'robot_description': robot_description},
-                    controller_params_file]
+        parameters=[controller_params_file],
+        remappings=[('~/robot_description', '/robot_description')]
     )
-
+   
+    # Add delay before starting controller manager
     delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
 
+    # Spawning the diff drive controller
     diff_drive_spawner = Node(
         package="controller_manager",
-        executable="spawner.py",
-        arguments=["diff_cont"],
+        executable="spawner",
+        arguments=["diff_cont"]
     )
-
     delayed_diff_drive_spawner = RegisterEventHandler(
         event_handler=OnProcessStart(
             target_action=controller_manager,
@@ -72,10 +42,11 @@ def generate_launch_description():
         )
     )
 
+    # Spawning the joint broadcaster
     joint_broad_spawner = Node(
         package="controller_manager",
-        executable="spawner.py",
-        arguments=["joint_broad"],
+        executable="spawner",
+        arguments=["joint_broad"]
     )
 
     delayed_joint_broad_spawner = RegisterEventHandler(
@@ -84,32 +55,21 @@ def generate_launch_description():
             on_start=[joint_broad_spawner],
         )
     )
-
-
-    # Code for delaying a node (I haven't tested how effective it is)
-    # 
-    # First add the below lines to imports
-    # from launch.actions import RegisterEventHandler
-    # from launch.event_handlers import OnProcessExit
-    #
-    # Then add the following below the current diff_drive_spawner
-    # delayed_diff_drive_spawner = RegisterEventHandler(
-    #     event_handler=OnProcessExit(
-    #         target_action=spawn_entity,
-    #         on_exit=[diff_drive_spawner],
-    #     )
+    # # Keyboard-based teleop control
+    # teleop_twist_keyboard_node = Node(
+    #     package="teleop_twist_keyboard",
+    #     executable="teleop_twist_keyboard",
+       
+    #     output="screen"
+        
     # )
-    #
-    # Replace the diff_drive_spawner in the final return with delayed_diff_drive_spawner
 
-
-
-    # Launch them all!
     return LaunchDescription([
         rsp,
-        # joystick,
-        twist_mux,
         delayed_controller_manager,
+        #diff_drive_spawner,
         delayed_diff_drive_spawner,
+        #joint_broad_spawner,
         delayed_joint_broad_spawner
+        # teleop_twist_keyboard_node,  # Add keyboard teleop node
     ])
